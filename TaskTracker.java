11084d4ab091e05efa1d0314977377ed631bd047
harsh tripathi <2k24.cs1i.2411233@gmail.com>
2026-04-22 12:10:56 +0530

    implemented list command in which it can show task based on filter todo, in progress,done

--- TaskTracker.java
+++ TaskTracker.java
@@ -20,10 +20,20 @@ public class TaskTracker {
     try{
       ensureFileExists();
 
-      if(command.equalsIgnoreCase("add")&& args.length>1){
-        handleAddTask(args[1]);
-      }else{
-        System.out.println("Unknown command or missing description.");
+      switch(command.toLowerCase()){
+        case "add":
+          if(args.length>1){
+            handleAddTask(args[1]);
+          }else{
+            System.out.println("Error: Missing description for add command.");
+          }
+          break;
+        case "list":
+          String filter=(args.length>1)?args[1]:null;
+          handleListTasks(filter);
+          break;
+        default:
+          System.out.println("Unknown command. Try 'add' or 'list'.");
       }
     }catch(Exception e){
       System.out.println("Error: " + e.getMessage());
@@ -58,6 +68,61 @@ public class TaskTracker {
 
     Files.writeString(Paths.get(FILE_PATH),UpdatedContent);
     System.out.println("Task added successfully (ID: " + id + ")");
-
   }
+
+  public static void handleListTasks(String filter) throws IOException{
+    String content=Files.readString(Paths.get(FILE_PATH));
+
+    if(content.equals("[]") ||content.isBlank()){
+      System.out.println("No task Found.");
+      return;
+    }
+
+    String cleaned=content.substring(1,content.length()-1);
+
+    String [] taskStrings=cleaned.split("\\},");
+
+    System.out.println("ID  | Status      | Description");
+    System.out.println("-----------------------------------");
+
+    for(String taskStr:taskStrings)
+    {
+      if(!taskStr.endsWith("}")){
+        taskStr+="}";
+      }
+
+      String id=extractValue(taskStr,"id");
+      String desc=extractValue(taskStr,"description");
+      String status=extractValue(taskStr,"status");
+
+      if(filter ==null ||status.equals(filter))
+      {
+        System.out.printf("%-3s | %-11s | %s%n", id, status, desc);
+      }
+    }
+  }
+
+  public static String extractValue(String json,String key){
+    String search="\""+key+"\":";
+    int start=json.indexOf(search);
+    if(start==-1)return "";
+
+    start+=search.length();
+
+    if(json.charAt(start)=='"'){
+      int end=json.indexOf("\"",start+1);
+      return json.substring(start+1,end);
+    }else{
+      int endComma=json.indexOf(",",start);
+      int endBrace=json.indexOf("}",start);
+
+      int end = (endComma != -1 && endComma < endBrace) ? endComma : endBrace;
+        return json.substring(start, end).trim();
+    }
+
+
+
+
+    }
+
 }
